d8d3ce419c2620d720ded66323c653a37da8ede6
alexwen.creates@gmail.com <alexwen.creates@gmail.com>
2018-01-21 02:22:56 -0600

    Remove media.

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,6 @@ class Home extends React.Component {
 
   state = {
     longform: null,
-    media: null,
   }
 
   componentWillMount() {
@@ -31,47 +30,21 @@ class Home extends React.Component {
           longform: response.results,
         });
       });
-      props.prismicCtx.api.query(Prismic.Predicates.at('document.type', 'media'), { orderings : '[my.media.date desc]' }, (err, response) => {
-        this.setState({ 
-          media: response.results,
-        });
-      });
-      
     }
   }
 
-  renderLongform = (longformArray, mediaArray) => {
-    if (longformArray && mediaArray) {
-      let array = longformArray.concat(mediaArray)
-      return array.map((post) => {
-        console.log(post)
-        if (post.type == 'longform') {
-          return (
-            <div className="parentContainer">
-              <a className="noUnderline" href={'/longform/' + post.uid}><div className="longformContainer">
-                  <img alt="cover" src={post.data.background.url} />
-                  <div className="titleContainer">
-                    <h2>{PrismicReact.RichText.asText(post.data.subtitle)}</h2>
-                    <h1>{PrismicReact.RichText.asText(post.data.title)}</h1>
-                  </div>
-              </div></a>
-            </div>
-          )
-        } else if (post.type == 'media') {
-          return (
-            <div className="parentContainer">
-              <div className="mediaContainer">
-                  <img alt="cover" src={post.data.cover.url} />
-                  <div className="titleContainer">
-                    <div className="titleOverlay"></div>
-                    <a className="noUnderline" href={'/media/' + post.uid}><h1>{PrismicReact.RichText.asText(post.data.name)}</h1></a>
-                    {PrismicReact.RichText.render(post.data.summary, this.props.prismicCtx.linkResolver)}
-                  </div>
+  renderLongform = (longformArray) => {
+    if (longformArray) {
+      return longformArray.map((longform) => 
+        <div className="parentContainer">
+          <a className="noUnderline" href={'/longform/' + longform.uid}><div className="longformContainer">
+              <img alt="cover" src={longform.data.background.url} />
+              <div className="titleContainer">
+                <h2>{PrismicReact.RichText.asText(longform.data.subtitle)}</h2>
+                <h1>{PrismicReact.RichText.asText(longform.data.title)}</h1>
               </div>
-            </div>
-          )
-        }
-      }
+          </div></a>
+        </div>
       )
     }
   }
@@ -80,7 +53,7 @@ class Home extends React.Component {
     return (
       <div>
         <h1 className="subHead">Pineapples & Cutty Sark</h1>
-        <div>{this.renderLongform(this.state.longform, this.state.media)}</div>
+        <div>{this.renderLongform(this.state.longform)}</div>
       </div>
     )
   }
